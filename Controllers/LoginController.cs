6a4abe462c8be5daf56b689694d152206d84b0b8
vitor <vitorsalves0812@gmail.com>
2023-12-16 17:19:10 -0300

    Adicionado captcha turnstile no form de login

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
 using VagasDoc.Helper;
 using VagasDoc.Models;
 using VagasDoc.Repository;
@@ -9,10 +10,12 @@ namespace VagasDoc.Controllers
     {
         private readonly IUsuarioRepository _usuariosRepository;
         private readonly ISessao _sessao;
-        public LoginController(IUsuarioRepository usuarioRepository, ISessao sessao)
+        private readonly IConfiguration _configuration;
+        public LoginController(IUsuarioRepository usuarioRepository, ISessao sessao, IConfiguration configuration)
         {
             _usuariosRepository = usuarioRepository;
             _sessao = sessao;
+            _configuration = configuration;
         }
         public IActionResult Index()
         {
@@ -21,14 +24,33 @@ namespace VagasDoc.Controllers
         }
 
         [HttpPost]
-        public IActionResult Entrar(LoginModel loginModel)
+        public async Task<IActionResult> EntrarAsync(IFormCollection form)
         {
+            var token = form["cf-turnstile-response"];
+
+            if (!string.IsNullOrEmpty(token) )
+            {
+                var tokenValido = await ValidaCaptcha(token);
+                if (!tokenValido)
+                {
+                    TempData["MensagemErro"] = "Captcha inválido.";
+                    return RedirectToAction("Index");
+                }
+            }
+            else
+            {
+                TempData["MensagemErro"] = "Captcha inválido.";
+                return RedirectToAction("Index");
+            }
+
             try
             {
-                UsuarioModel usuario = _usuariosRepository.BuscarPorLogin(loginModel.Login);
+                var login = form["Login"];
+                var senha = form["Senha"];
+
+                UsuarioModel usuario = _usuariosRepository.BuscarPorLogin(login);
                 if (usuario != null)
                 {
-                    var senha = loginModel.Senha;
                     senha = Cripto.Encrypt(senha);
 
                     if (usuario.SenhaValida(senha))
@@ -51,6 +73,36 @@ namespace VagasDoc.Controllers
                 return RedirectToAction("Index");
             }
 
+        }
+
+        public async Task<bool> ValidaCaptcha(string token)
+        {
+            var url = "https://challenges.cloudflare.com/turnstile/v0/siteverify";
+
+            var secret = _configuration["Keys:Turnstile.Captcha.Secret"];
+
+            var formValues = new Dictionary<string, string>
+            {
+                ["secret"] = secret,
+                ["response"] = token
+            };
+
+            var formData = new HttpRequestMessage(HttpMethod.Post, url)
+            {
+                Content = new FormUrlEncodedContent(formValues)
+            };
+
+            HttpClient httpClient = new HttpClient();
+
+            var request = await httpClient.SendAsync(formData);
+            var retornoCaptcha = await request.Content.ReadFromJsonAsync<CaptchaModel>();
+
+            if (retornoCaptcha.Success)
+            {
+                return true;
+            }
+
+            return false;
         }
 
         public IActionResult Sair()
